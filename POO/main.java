606c1da9595cafe041c5ac83768837315a7453ab
JoshuaObando <JOSHUA.OBANDOGONZALEZ@ucr.ac.cr>
2024-05-05 23:10:35 -0600

    avance

--- POO/main.java
+++ POO/main.java
@@ -3,6 +3,6 @@ package POO;
 public class main {
 
     public static void main(String[] args) {
-        System.out.println("hola");
+        System.out.println("hola"); System.out.println("Bye");
     }
 }
