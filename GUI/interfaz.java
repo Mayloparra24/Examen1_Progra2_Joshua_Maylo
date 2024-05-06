8c1331878f1baef03134439087e426e27bc193e9
MayloParra <a494256@gmail.com>
2024-05-06 00:16:30 -0600

    Jframe

--- GUI/interfaz.java
+++ GUI/interfaz.java
@@ -1,13 +1,19 @@
 package GUI;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class interfaz extends JFrame{
     
-
+    JPanel principal;
     public interfaz (){
+    principal=new JPanel();
+    add(principal);
+      Jframe();
 
+    }
 
+    public void Jframe(){
         setSize(1000,700);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
